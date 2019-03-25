c05b332c2bb49a188a629cf98dd1669f94879329
vsofat <vsofat00@stuy.edu>
2019-03-25 19:53:30 -0400

    merge from both ends

--- Merge.java
+++ Merge.java
@@ -12,11 +12,36 @@ public class Merge{
   if(low >= high){
      return;
    }
-   int pivot = ((high - low) / 2) ; // finds center (similar to optimized quick)
-   mergesort(data, low, pivot); //mergesort left side
-   mergesort(data, pivot, high); //mergesort right side
+   int pivot = ((high - low) / 2) + low; // finds center (similar to optimized quick)
 
-   // missing merge part
+   // mergesort(data, low, pivot); //mergesort left side
+   //  mergesort(data, pivot, high); //mergesort right side
+
+   int leftInd = low;
+   int rightInd = pivot;
+
+   for(int ind = 0; ind < data.length; ind++){
+      if(leftInd < pivot && rightInd < high){ //there're numbers still left in each side
+        if(data[leftInd] <= data[rightInd]){
+          data2[ind] = data[leftInd];
+          leftInd++;
+        }
+        else{
+          data2[ind] = data[rightInd];
+          rightInd++;
+        }
+      }
+      else{
+        if(leftInd < pivot){
+          data2[ind] = data[leftInd];
+          leftInd++;
+        }
+        else{
+          data2[ind] = data[rightInd];
+          rightInd++;
+        }
+      }
+    }
 
     // Pseudo code
     /*mergesort(data,lo,hi):
@@ -30,9 +55,9 @@ public class Merge{
   }
 
   public static void swap(int[] ary, int one, int two){
-   int temp = ary[one];
+   int data2 = ary[one];
    ary[one] = ary[two];
-   ary[two] = temp;
+   ary[two] = data2;
  }
 
 
